fa03eba2164b2e2f0b60fa96f3b8885b1d7dc502
Kolya <vkolya0@gmail.com>
2025-08-29 20:10:45 +0300

    test deptrac

--- src/Catalogue/Application/Command/Handler/CreateProductCommandHandler.php
+++ src/Catalogue/Application/Command/Handler/CreateProductCommandHandler.php
@@ -5,9 +5,9 @@ namespace App\Catalogue\Application\Command\Handler;
 use App\Catalogue\Application\Command\CreateProductCommand;
 use App\Catalogue\Domain\Entity\Product;
 use App\Catalogue\Domain\Repository\ProductRepositoryInterface;
+use App\Order\Application\Command\CreateOrderCommand;
 use App\SharedKernel\Domain\Persistence\TransactionRunnerInterface;
 use App\SharedKernel\Domain\ValueObject\Money;
-
 class CreateProductCommandHandler
 {
     public function __construct(private ProductRepositoryInterface $productRepository, private TransactionRunnerInterface $transactionRunner)
@@ -16,6 +16,7 @@ class CreateProductCommandHandler
 
     public function __invoke(CreateProductCommand $command): Product
     {
+        $c = new CreateOrderCommand('12', 1, []);
         return $this->transactionRunner->run(function () use ($command) {
             $product = Product::create($command->id, $command->name, Money::fromInt($command->price), $command->onHand);
             $this->productRepository->add($product);
